0f79fe9b36e549e1bcdab017c02ddacd8296b70d
putpix <putpix@gmail.com>
2017-06-03 22:40:06 +0300

    Implement extension + readme

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-Simple extension that makes it easier to check car on mnt.ee.
-Supported sites:
-    auto24.ee:  Open any car link, make Reg.number or VIN visible
-                and you'll get links to mnt.ee.
-                If you make both links visible - you'll get direct link to check by reg.number and vin.
+## Simple extension that makes it easier to check car on mnt.ee
+
+---
+## How to install extension?
+* Download this repository
+* Click More -> More tools -> Extensions
+* Drag and drop folder with extension on opened page
+---
+## Supported sites
+* #### auto24.ee
+    * Open any car link, make Reg.number or VIN visible and you'll get links to mnt.ee.
+    * If you make both links visible - you'll get direct link to check by reg.number and vin.
---
+++ scripts.js
@@ -0,0 +1,92 @@
+var autoRegNumberTr = ".field-reg_nr";
+var vinNumberTr = ".field-tehasetahis";
+
+function onPageChanged() {
+	var autoRegNumber = findAutoRegNumber();
+	var autoVinNumber = findAutoVinNumber();
+	if (autoRegNumber && autoVinNumber) {
+		console.log("Add check by reg and vin number");
+		var marker = buildClassMarker("regandvin");
+		if (noMarker(marker)) {
+			$("<tr>" +
+				"<td class='label'>mnt.ee:</td>" +
+				"<td class='value'>" +
+				"<a class='" + marker + "' " +
+				"href='" + buildCheckCarLink(autoRegNumber, autoVinNumber) + "'>" + chrome.i18n.getMessage("checkByRegAndVinNumbers") + "</a></td>" +
+				"</tr>").insertAfter($(vinNumberTr));
+		}
+	}
+}
+
+function findAutoRegNumber() {
+	return findNumberAndAddLinkToCheck(autoRegNumberTr, "REG", "checkByRegNumberOnly", function (autoRegNumber) {
+		return buildCheckCarLink(autoRegNumber, null);
+	});
+}
+
+function findAutoVinNumber() {
+
+	return findNumberAndAddLinkToCheck(vinNumberTr, "VIN", "checkByVinNumberOnly", function (autoVinNumber) {
+		return buildCheckCarLink(null, autoVinNumber);
+	});
+}
+
+function buildClassMarker(uniqueIdentifier) {
+	return "auto24mnteecheck" + uniqueIdentifier;
+}
+
+function noMarker(classMarker) {
+	return !$("." + classMarker).exists();
+}
+function findNumberAndAddLinkToCheck(baseTagElement, tagType, localizeMsg, linkByValueBuilder) {
+	var classMarker = buildClassMarker(tagType);
+	var autoNumber = null;
+	var numberTr = $(baseTagElement);
+	if (numberTr.exists()) {
+		if (numberTr.find(".preview").exists()) {
+			console.trace("Preview of " + tagType + " number loaded, nothing to do here");
+		} else {
+			var numberValueSpan = numberTr.find(".value");
+			if (numberValueSpan.exists()) {
+				autoNumber = numberValueSpan.text();
+				if (noMarker(classMarker)) {
+					console.log("Real " + tagType + " number loaded, number: " + autoNumber);
+					$("<br><a class='" + classMarker + "' href='" + linkByValueBuilder(autoNumber) + "'>" + chrome.i18n.getMessage(localizeMsg) + "</a>")
+						.insertAfter(numberValueSpan);
+				}
+			}
+		}
+	}
+	return autoNumber
+}
+
+
+function buildCheckCarLink(regNumber, vin) {
+	var base = "https://eteenindus.mnt.ee/public/soidukTaustakontroll.jsf?1=1";
+	if (regNumber) {
+		base += "&regMark=" + regNumber
+	}
+	if (vin) {
+		base += "&vin=" + vin
+	}
+	return base
+}
+
+
+$.fn.exists = function () {
+	return this.length !== 0;
+};
+
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function (mutations, observer) {
+	onPageChanged()
+});
+
+observer.observe(document, {
+	subtree: true,
+	attributes: true
+});
+
+
+console.log("Extension loaded on page");
